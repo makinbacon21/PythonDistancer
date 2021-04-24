b9c9c705f979d41cbe4834c82f3bc174cae802d3
= <=>
2021-04-23 21:29:23 -0400

    init

---
+++ algo.py
@@ -0,0 +1,21 @@
+# algo.py
+import click
+import googlemaps
+import pandas as pd
+from itertools import tee
+
+@click.command()
+def main():
+    df = pd.read_csv('go_track_trackspoints.csv', sep=';')
+    gmaps = googlemaps.Client(key='AIzaSyAZs33KAQ57SeZ6HJJYd-4nFisiFTnES94')
+
+    for row in df.iterrows():
+        list = []
+        for row2 in df.iterrows():
+            result = gmaps.distance_matrix(origins=row[0], destinations=row2[0], mode="driving")["rows"][0]["elements"][0]["distance"]["value"]
+            list.append(result)    
+    
+    click.echo(list)
+
+if __name__ == "__main__":
+    main()
